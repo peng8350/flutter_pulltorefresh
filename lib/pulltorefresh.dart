b353d1cef684fe6a9fdf4dd78fe9ca456f2dfff5
Jpeng <peng8350@gmail.com>
2018-05-01 14:20:46 +0800

    Add NotificationListener and SizeTranstion to implements basic of Expand

--- lib/pulltorefresh.dart
+++ lib/pulltorefresh.dart
@@ -2,7 +2,7 @@ library pulltorefresh;
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class SmartRefresher extends StatefulWidget {
   /*
@@ -16,11 +16,13 @@ class SmartRefresher extends StatefulWidget {
   //This bool will affect whether or not to have the function of drop-down refresh.
   final bool enablePulldownRefresh;
 
+  final double triggerDistance;
+
   SmartRefresher(
       {@required this.child,
       this.enablePulldownRefresh: true,
       this.enablePullUpLoad: false,
-      this.header,
+      this.header,this.triggerDistance:150.0,
       this.footer})
       : assert(child != null);
 
@@ -28,16 +30,49 @@ class SmartRefresher extends StatefulWidget {
   _SmartRefresherState createState() => new _SmartRefresherState();
 }
 
-class _SmartRefresherState extends State<SmartRefresher> {
+class _SmartRefresherState extends State<SmartRefresher>
+    with TickerProviderStateMixin {
+  AnimationController _headerController, _footerController;
+  double startDragY=0.0;
+
+  void _disapperate(){
+    if(!_headerController.isDismissed)
+    _headerController.animateTo(0.01,curve: new Cubic(0.0, 0.0, 1.0, 1.0),duration: const Duration(milliseconds: 100));
+    if(!_footerController.isDismissed)
+      _footerController.animateTo(0.01,curve: new Cubic(0.0, 0.0, 1.0, 1.0),duration: const Duration(milliseconds: 100));
+  }
+
+  bool _onScrollUpdate(ScrollNotification notification) {
+    if(notification is ScrollStartNotification){
+      startDragY = notification.dragDetails.globalPosition.dy;
+    }
+    if(notification is OverscrollNotification){
+        // This means that the distance that the user dragged occupies the height of the control.
+        final ratio = ((notification.dragDetails.globalPosition.dy-startDragY)/context.size.height)*7.0;
+        print(ratio);
+        _headerController.value= ratio;
+    }
+    if(notification is ScrollEndNotification){
+        _disapperate();
+    }
+
+
+
+    return false;
+  }
+
+  bool _isiOS(){
+    return Theme.of(context).platform==TargetPlatform.iOS;
+  }
+
   // if your renderHeader null, it will be replaced by it
   Widget _buildDefaultHeader() {
     return new Container(
-      color:const Color(0xffdddddd),
-      height: 50.0,
+      height:50.0,
       child: new Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          new CupertinoActivityIndicator(),
+          const CupertinoActivityIndicator(),
           new Text('Refreshing....')
         ],
       ),
@@ -47,13 +82,11 @@ class _SmartRefresherState extends State<SmartRefresher> {
   // if your renderFooter null, it will be replaced by it
   Widget _buildDefaultFooter() {
     return new Container(
-      color:const Color(0xffdddddd),
-      alignment: Alignment.center,
       height: 50.0,
       child: new Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          new CupertinoActivityIndicator(),
+          const CupertinoActivityIndicator(),
           new Text('LoadMore....')
         ],
       ),
@@ -61,13 +94,43 @@ class _SmartRefresherState extends State<SmartRefresher> {
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+  
+  @override
   Widget build(BuildContext context) {
-    return new ListView(
-      children: <Widget>[
-        _buildDefaultHeader(),
-        widget.child,
-        _buildDefaultFooter()
-      ],
+
+    if(_isiOS()){
+
+      _headerController = new AnimationController(vsync: this,duration: const Duration(milliseconds: 200),lowerBound: 0.0,upperBound: 25.0);
+      _footerController = new AnimationController(vsync: this,duration: const Duration(milliseconds: 200),lowerBound: 0.0,upperBound: 1.0);
+    }
+    else{
+
+    }
+
+    return new NotificationListener(
+      child: new ListView(
+        physics: new ClampingScrollPhysics(),
+        children: <Widget>[
+
+         new Container(color:const Color(0xffdddddd),child:  new SizeTransition(
+
+             axisAlignment: 1.0,
+             sizeFactor: _headerController,
+             child: _buildDefaultHeader()),),
+          widget.child,
+          new SizeTransition(
+              axisAlignment: -1.0,
+              sizeFactor: _headerController,
+              child: _buildDefaultFooter())
+        ],
+      ),
+      onNotification: _onScrollUpdate,
     );
   }
+  
+  
 }
