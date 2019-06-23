52698444915f5993d19555230d607de1a8c0e55b
Jpeng <peng8350@gmail.com>
2019-06-23 12:19:47 +0800

    Revert "Fix error judgement in parent physics,lead to  #111"
    This reverts commit 858afa55

--- lib/src/internals/refresh_physics.dart
+++ lib/src/internals/refresh_physics.dart
@@ -4,23 +4,11 @@
     createTime:2018-05-02 14:39
  */
 
-import 'package:flutter/physics.dart';
 import 'package:flutter/widgets.dart';
 
-/*
-  use to add other scroll effect
-  only support three common physics:
-  1.  AlwaysScrollPhysics(default),
-  2. ClampedScrollPhysics,
-  3. BouncingScrollPhysics
-*/
-
 class RefreshPhysics extends ScrollPhysics {
-
-  bool _clamped = false;
-
-  RefreshPhysics({ScrollPhysics parent}) : super(parent: parent);
-
+  /// Creates scroll physics that bounce back from the edge.
+  const RefreshPhysics({ScrollPhysics parent}) : super(parent: parent);
 
   @override
   RefreshPhysics applyTo(ScrollPhysics ancestor) {
@@ -36,8 +24,7 @@ class RefreshPhysics extends ScrollPhysics {
   @override
   double applyBoundaryConditions(ScrollMetrics position, double value) {
     // TODO: implement applyBoundaryConditions
-    // if createBallisticSimulation() return ClampingScrollSimulation,it should be stopped before scrollView bounce out of visual area
-    if(_clamped) {
+    if(parent is ClampingScrollPhysics) {
       if (value < position.minScrollExtent &&
           position.minScrollExtent < position.pixels) { // hit top edge
         return value - position.minScrollExtent;
@@ -45,8 +32,9 @@ class RefreshPhysics extends ScrollPhysics {
       if (position.pixels < position.maxScrollExtent &&
           position.maxScrollExtent < value) // hit bottom edge
         return value - position.maxScrollExtent;
+      if (position.maxScrollExtent <= position.pixels && position.pixels < value) // overscroll
+        return value - position.pixels;
     }
-
     return 0.0;
   }
 
@@ -64,10 +52,7 @@ class RefreshPhysics extends ScrollPhysics {
         tolerance: tolerance,
       );
     }
-    Simulation parentSimulation = super.createBallisticSimulation(position, velocity);
-
-    _clamped =  parentSimulation is ClampingScrollSimulation;
-    return parentSimulation;
+    return super.createBallisticSimulation(position, velocity);
   }
 
 }
