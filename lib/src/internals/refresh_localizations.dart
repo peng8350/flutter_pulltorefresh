173788db7efbc71a092aed4dcad5681e8a8bbc1c
meketiuk <serhii.meketiuk@gmail.com>
2019-09-25 20:05:57 +0200

    Change Ukrainian Country Code

--- lib/src/internals/refresh_localizations.dart
+++ lib/src/internals/refresh_localizations.dart
@@ -42,7 +42,7 @@ class RefreshLocalizations {
     'zh': ChRefreshString(),
     'fr': FrRefreshString(),
     'ru': RuRefreshString(),
-    'ua': UaRefreshString()
+    'uk': UkRefreshString()
   };
 
   RefreshString get currentLocalization {
@@ -66,7 +66,7 @@ class RefreshLocalizationsDelegate
 
   @override
   bool isSupported(Locale locale) {
-    return ['en', 'zh', 'fr', 'ru', 'ua'].contains(locale.languageCode);
+    return ['en', 'zh', 'fr', 'ru', 'uk'].contains(locale.languageCode);
   }
 
   @override
@@ -262,7 +262,7 @@ class RuRefreshString implements RefreshString {
 }
 
 // Ukrainian
-class UaRefreshString implements RefreshString {
+class UkRefreshString implements RefreshString {
   @override
   String canLoadingText = "Відпустіть, щоб завантажити більше";
 
