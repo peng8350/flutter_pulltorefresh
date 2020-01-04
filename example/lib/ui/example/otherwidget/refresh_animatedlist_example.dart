fbfeabad973b8097fe5e1c5d0fb2b200d162d8fa
Jpeng <peng8350@gmail.com>
2020-01-04 19:43:34 +0800

    fix demo compile error

--- example/lib/ui/example/otherwidget/refresh_animatedlist_example.dart
+++ example/lib/ui/example/otherwidget/refresh_animatedlist_example.dart
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-import 'package:example/other/refresh_animatedlist.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
