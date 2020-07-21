593b7adad4522d1d034af24dff6686f91bc72cf5
peng8350 <peng8350@gmail.com>
2020-07-21 17:43:18 +0800

    fix #308

--- example/lib/ui/example/useStage/Nested.dart
+++ example/lib/ui/example/useStage/Nested.dart
@@ -118,7 +118,7 @@ class _RefreshListViewState extends State<RefreshListView> {
 
   void _onLoading() async {
     // monitor network fetch
-    await Future.delayed(Duration(milliseconds: 1000));
+    await Future.delayed(Duration(milliseconds: 250));
     // if failed,use loadFailed(),if no data return,use LoadNodata()
     items.add((items.length + 1).toString());
     if (mounted) setState(() {});
